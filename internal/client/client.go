3bebfd9068bb856653f21817c36f95099b6c5d0d
Sergey Borovoy <sergey@katobr.com>
2026-06-12 11:43:21 +0300

    Increased API timeouts

--- internal/client/client.go
+++ internal/client/client.go
@@ -78,7 +78,7 @@ func NewQratorClient(apiKey, endpoint string, debug bool) *QratorClient {
 			KeepAlive: 30 * time.Second,
 		}).DialContext,
 		TLSHandshakeTimeout:   10 * time.Second,
-		ResponseHeaderTimeout: 10 * time.Second,
+		ResponseHeaderTimeout: 120 * time.Second,
 		ExpectContinueTimeout: 1 * time.Second,
 		MaxIdleConns:          100,
 		IdleConnTimeout:       90 * time.Second,
@@ -86,7 +86,7 @@ func NewQratorClient(apiKey, endpoint string, debug bool) *QratorClient {
 	}
 
 	client := &http.Client{
-		Timeout:   30 * time.Second,
+		Timeout:   120 * time.Second,
 		Transport: transport,
 	}
 
